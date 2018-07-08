a41dccf8ec2999ce695e42f76ad49aa718129ca0
Krishnan <snkrishnan1@yahoo.co.in>
2018-07-07 22:48:03 -0400

    first commit of source code

---
+++ PowerBIEmbedded_AppOwnsData/Models/ReportDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PowerBIEmbedded_AppOwnsData.Models
+{
+    public class ReportDetails
+    {
+        public string ReportID { get; set; }
+
+        public string ReportName { get; set; }
+
+        public string DatasetID { get; set; }
+
+        public string LastRefreshDate { get; set; }
+
+        public string EmbedURL { get; set; }
+
+        public string WebUrl { get; set; }
+    }
+}
---
+++ PowerBIEmbedded_AppOwnsData/Models/ReportEmbedToken.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PowerBIEmbedded_AppOwnsData.Models
+{
+    public class ReportEmbedToken
+    {
+        public string ReportID { get; set; }
+
+        public string ReportName { get; set; }
+
+        public string DatasetID { get; set; }
+
+        public string LastRefreshDate { get; set; }
+
+        public EmbedToken EmbedToken { get; set; }
+
+        public string EmbedTokenExpiryTime { get; set; }
+
+        public string EmbedURL { get; set; }
+
+        public string WebUrl { get; set; }
+    }
+}
